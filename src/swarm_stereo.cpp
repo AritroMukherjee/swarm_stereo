468847d0acf2eaa92bf0476565b95c7d5759cf66
dementor <soumyadeep9@gmail.com>
2015-06-18 12:03:48 +0200

    FPS Log added

--- src/swarm_stereo.cpp
+++ src/swarm_stereo.cpp
@@ -2,13 +2,37 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
-  
+#include <time.h> 
+
+
+// fps counter begin
+time_t start, end;
+int counter = 0;
+double sec;
+double fps;
+// fps counter end
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
     try
     {
+		// fps counter begin
+        if (counter == 0){
+            time(&start);
+        }
+        // fps counter end
 		cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-		cv::waitKey(30);
+		// fps counter begin
+        time(&end);
+        counter++;
+        sec = difftime(end, start);
+        fps = counter/sec;
+        if (counter > 30)
+            printf("%.2f fps\n", fps);
+        // overflow protection
+        if (counter == (INT_MAX - 1000))
+            counter = 0;	
+		cv::waitKey(10);
     }
     catch (cv_bridge::Exception& e)
     {
@@ -24,6 +48,8 @@ int main(int argc, char **argv)
     cv::startWindowThread();
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber sub = it.subscribe("/ros_pleora/image_raw", 1, imageCallback);
+
+	
 
 	while(nh.ok())
 		ros::spin();
